a036f9c40a8f57bf49b550ef9d2cbc3123c4faea
NaldonStudy <naldonstudy@gmail.com>
2025-02-10 11:54:05 +0900

    [Bronze I] Title: 알람시계, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/3154. 알람시계/알람시계.java
+++ 백준/Bronze/3154. 알람시계/알람시계.java
@@ -1,48 +1,63 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static final int[] XCOOR = { 1, 0, 1, 2, 0, 1, 2, 0, 1, 2 };
-    static final int[] YCOOR = { 3, 0, 0, 0, 1, 1, 1, 2, 2, 2 };
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] N = sc.nextLine().split(":");
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+		int[] results = new int[2];
 
-        int inH = Integer.parseInt(st.nextToken());
-        int inM = Integer.parseInt(st.nextToken());
+		List<List<Integer>> lists = new ArrayList<>();
 
+		int[][] keypad = { { 3, 1 }, { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
+				{ 2, 2 } };
 
-        int H = inH % 24;
-        int M = inM % 60;
-        int minCnt = Integer.MAX_VALUE, minH = 0, minM = 0;
+		for (int i = 0; i < 2; i++) {
+			int time = 0;
+			int c = (i == 0) ? 24 : 60;
+			int tmp_rest = Integer.parseInt(N[i]) % c;
+			
+			List<Integer> tmp_list = new ArrayList<>();
+			
+			for(int j = 0; time < 100; j++) {
+				time = (c*j) + tmp_rest;
+				if(time >= 100) break;
+				tmp_list.add(time);
+			}
+			
+			lists.add(tmp_list);
+			
+		}
+		
+		int cases = lists.get(0).size() * lists.get(0).size();
+		int min_dis = 100;
+		for(int i = 0; i <  lists.get(0).size(); i++) {
+			int n2 = lists.get(0).get(i) % 10;
+			int n1 = (int)((lists.get(0).get(i) / 100.0) * 10.0);
+			int cur_dis;
+			for(int j = 0; j <  lists.get(1).size(); j++) {
+				int n4 = lists.get(1).get(j) % 10;
+				int n3 = (int)((lists.get(1).get(j) / 100.0) * 10.0);
+				
+				cur_dis = Math.abs(keypad[n1][0] - keypad[n2][0]) + Math.abs(keypad[n1][1] - keypad[n2][1]) +
+				Math.abs(keypad[n2][0] - keypad[n3][0]) + Math.abs(keypad[n2][1] - keypad[n3][1]) +
+				Math.abs(keypad[n3][0] - keypad[n4][0]) + Math.abs(keypad[n3][1] - keypad[n4][1]);
+				
+				if(min_dis > cur_dis) {
+					min_dis = cur_dis;
+					
+					results[0] = lists.get(0).get(i);
+					results[1] = lists.get(1).get(j);
+				}
+			}
+		}
+		
+		System.out.printf("%02d:%02d" ,results[0], results[1]);
 
-        for (int i = H; i < 100; i += 24) {
-            for (int j = M; j < 100; j += 60) {
-                int[] numArr = { i / 10, i % 10, j / 10, j % 10 };
-                int cnt = 0;
-
-                for (int k = 0; k < 3; k++) {
-                    cnt += Math.abs(XCOOR[numArr[k]] - XCOOR[numArr[k + 1]]);
-                    cnt += Math.abs(YCOOR[numArr[k]] - YCOOR[numArr[k + 1]]);
-                }
-
-                if (cnt < minCnt) {
-                    minCnt = cnt;
-                    minH = i;
-                    minM = j;
-                }
-            }
-        }
-
-        bw.write(String.format("%02d:%02d", minH, minM));
-        bw.flush();
-        bw.close();
-    }
+		
+	}
 }
