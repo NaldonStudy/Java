46ec4f809107f3f73cfad299c7e51d90d77d12c5
NaldonStudy <naldonstudy@gmail.com>
2025-03-16 20:12:31 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static StringBuilder sb;
+	static int[] numArr, resArr;
+	static boolean[] visit;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		numArr = new int[N];
+		resArr = new int[M];
+		visit = new boolean[N];
+		
+		for(int i = 0; i < N; i++) {
+			numArr[i] = i+1;
+		}
+		sequence(0, 0);
+		bw.write(sb+"");
+		bw.flush();
+		bw.close();
+	}
+	
+	static void sequence(int idx, int sidx) {
+		if (sidx == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(resArr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = idx; i < N; i++) {
+			if(!visit[i]) {
+				resArr[sidx] = numArr[i];
+				sequence(i+1, sidx+1);
+			}
+		}
+		
+	}
+}
