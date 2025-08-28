0f6e58116260c80d503f54130ecd834788b29529
NaldonStudy <naldonstudy@gmail.com>
2025-08-28 17:25:52 +0900

    [Gold IV] Title: 연구소, Time: 1076 ms, Memory: 298300 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,123 @@
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[][] map;
+
+    static int res = Integer.MIN_VALUE;
+
+    // 상 하 좌 우
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    static public class Node {
+        int row, col;
+
+        Node (int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer NM = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(NM.nextToken());
+        M = Integer.parseInt(NM.nextToken());
+
+        map = new int[N][M];
+
+        for(int row = 0; row < N; row++) {
+            StringTokenizer inRow = new StringTokenizer(br.readLine());
+
+            for(int col = 0; col < M; col++) {
+                map[row][col] = Integer.parseInt(inRow.nextToken());
+            }
+        }
+
+        dfs(0);
+
+        bw.write("" + res);
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int depth) {
+        // 벽 3개 추가되었을 때
+        if(depth == 3) {
+            bfs();
+            return;
+        }
+
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                // 질병도, 벽도 아닌 일반 빈칸일 때
+                if(map[row][col] != 0) continue;
+
+                // 해당 위치가 0, 그니깐 아무것도 없을 때
+                map[row][col] = 1;
+                dfs(depth + 1);
+                map[row][col] = 0;
+            }
+        }
+    }
+
+    static void bfs() {
+        Queue<Node> q = new LinkedList<Node>();
+
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                if(map[row][col] == 2) {
+                    q.add(new Node(row, col));
+                }
+            }
+        }
+
+        int[][] copymap = new int[N][M];
+
+        for(int i = 0; i < N; i++) {
+            copymap[i] = Arrays.copyOf(map[i], M);
+        }
+
+        while(!q.isEmpty()) {
+            Node tempNode = q.poll();
+            int row = tempNode.row;
+            int col = tempNode.col;
+
+            for(int i = 0; i < 4; i++) {
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+
+                // 1. 범위 밖으로 나가면
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+
+                // 2. 갈수 없는곳. 그니깐 0이 아닌 딴거일 때
+                if (copymap[nr][nc] != 0) continue;
+
+                copymap[nr][nc] = 2;
+                q.add(new Node(nr, nc));
+            }
+        }
+
+        searchSafeArea(copymap);
+    }
+
+    static void searchSafeArea (int[][] inMap) {
+        int safeArea = 0;
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                if(inMap[row][col] != 0) continue;
+
+                safeArea++;
+            }
+        }
+
+        if(safeArea > res) res = safeArea;
+    }
+}
