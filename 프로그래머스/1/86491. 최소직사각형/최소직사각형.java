b531e42e514db1cb0b1ba8a70589a0f32b705dd0
김도훈 <naldonstudy@gmail.com>
2026-01-05 22:28:25 +0900

    [level 1] Title: 최소직사각형, Time: 1.94 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,35 +2,13 @@ import java.lang.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        int len = sizes.length;
-        int maxSize = 0;
-        int maxIdx = 0;
-        int maxWH = 0;
-        
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < 2; j++) {
-                if(sizes[i][j] > maxSize) {
-                    maxSize = sizes[i][j];
-                    maxIdx = i;
-                    maxWH = j;
-                }
-            }
+        int widthMax = 0;
+        int heightMax = 0;
+        for(int[] card : sizes) {
+            widthMax = Math.max(widthMax, Math.max(card[0], card[1]));
+            heightMax = Math.max(heightMax, Math.min(card[0], card[1]));
         }
-
-        int canLean = sizes[maxIdx][1-maxWH];
-        
-        loop:
-        for(int i = 0; i < len; i++) {
-            if(i == maxIdx) continue;
-            int w = sizes[i][0];
-            int h = sizes[i][1];
-            int smaller = Math.min(w, h);
-            canLean = Math.max(canLean, smaller);
-        }
-        
-        answer = canLean * maxSize;
-        
+        int answer = widthMax * heightMax;
         return answer;
     }
 }
